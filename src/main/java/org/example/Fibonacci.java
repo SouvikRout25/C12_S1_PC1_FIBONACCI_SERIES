16cf04b8a85c52a43151d4d19134ac1ac723b08f
SOUVIK ROUT <souvikrout.21@gmail.com>
2022-10-28 13:18:48 +0530

    initial

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -1,2 +1,22 @@
-package org.example;public class Fibonacci {
+package org.example;
+
+public class Fibonacci {
+
+    public long fibonacci(long number) {
+
+        if(number < 0){
+            return -1;
+        }
+        else if(number <= 1){
+            return number;
+        }
+        else {
+            return fibonacci(number-1) + fibonacci(number - 2);
+        }
+    }
+    public void printFibonacciSeries(long number){
+        for (int i = 0; i < number; i++) {
+            System.out.print(" " + fibonacci(i));
+        }
+    }
 }
--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -1,2 +1,37 @@
-package org.example;public class FibonacciTest {
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FibonacciTest {
+
+    Fibonacci fib = null;
+    @Before
+    public void setUp() throws Exception {
+        fib= new Fibonacci();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        fib = null;
+    }
+
+    @Test
+    public void test1() {
+        assertEquals( 1,fib.fibonacci(1));
+    }
+
+    @Test
+    public void test2(){
+        assertEquals(0,fib.fibonacci(0));
+    }
+
+    @Test
+    public void test3(){
+        assertEquals(-1,fib.fibonacci(-8));
+    }
+
 }
